7c65b05dfb16e776e066a528c8d6bd8b2b15ed7d
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2024-12-10 21:31:20 +0100

    implement a convenience function

--- extractingIris.py
+++ extractingIris.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import pandas as pd
 from perceptron import Perceptron
+from matplotlib.colors import ListedColormap
 
 s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 print ('From URL:', s)
@@ -43,3 +44,22 @@ plt.plot(range(1, len(ppn.errors_)+ 1),
 plt.xlabel('Epochs')
 plt.ylabel('Number of updates')
 plt.show()
+
+def plot_decision_regions(X, y, classifier, resolution = 0.02):
+    # setup marker generator and color map 
+    markes = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[len(np.unique(y))])
+
+    #plot the decision surpface
+    x1_min, x1_max = X[X:, 0].min() -1, X[:,0].max() +1
+    x2_min, x2_max = X[X:, 1].min() -1, X[:,1].max() +1
+
+    xx1, xx2=np.meshgrid(np.arange(x1_min, x1_max, resolution), 
+                         np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
